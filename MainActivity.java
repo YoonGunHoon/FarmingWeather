c066aa658fff3650b6f7384cb3ce0d0feb016ff4
kimsehoon <sehoon4222@naver.com>
2015-06-15 23:46:47 +0900

    메인엑티비티(수정함)
    메인화면 수정완료 최종

--- MainActivity.java
+++ MainActivity.java
@@ -3,9 +3,11 @@
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.*;
+import android.net.*;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.*;
 
 public class MainActivity extends Activity {
 
@@ -14,7 +16,18 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_main);
-	
+		//현재날씨
+		Button btn0 = (Button)findViewById(R.id.btn0);
+		btn0.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, WeatherActivity.class);
+				startActivity(intent);
+			}
+		});
+		
+		
+		//농업기술
 		Button btn1 = (Button)findViewById(R.id.btn1);
 		btn1.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -23,7 +36,7 @@ public class MainActivity extends Activity {
 				startActivity(intent);
 			}
 		});
-
+		//소득
 		Button btn2 = (Button)findViewById(R.id.btn2);
 		btn2.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -32,7 +45,7 @@ public class MainActivity extends Activity {
 				startActivity(intent);
 			}
 		});
-	
+		//병해충
 		Button btn3 = (Button)findViewById(R.id.btn3);
 		btn3.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -41,5 +54,20 @@ public class MainActivity extends Activity {
 				startActivity(intent);
 			}
 		});
+		Button btn4 = (Button)findViewById(R.id.btn4);
+		btn4.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			
+				Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://m.kma.go.kr/m/warning/warning_01.jsp"));
+	
+				startActivity(intent);
+			}
+		});
+		TextView tvMain1 = (TextView)findViewById(R.id.textView1);
+		tvMain1.setTypeface(Typeface.createFromAsset(getAssets(), "binpio2.ttf"));
+		TextView tvMain2 = (TextView)findViewById(R.id.textView2);
+		tvMain2.setTypeface(Typeface.createFromAsset(getAssets(), "sehoon.ttf"));
+		
 	}
 }
